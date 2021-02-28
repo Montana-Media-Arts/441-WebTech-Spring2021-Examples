fd6438d848dedd6dc2a7f9baccd2039e4ffc104d
Michael Cassens <michaelcassens@gmail.com>
2021-02-27 21:10:27 -0700

    updated with more comments

--- Week 7/HW7Live/scripts/main.js
+++ Week 7/HW7Live/scripts/main.js
@@ -1,17 +1,24 @@
+// let's create an empty array. We will add to this later
 var myViewFinderArray = new Array();
 
+// this is the main ViewFinder class
 class ViewFinder
 {
+    // the constructor requires only one title, the description, imagePath, etc.
+    // we will create multiple objects from this class
     constructor(title)
     {
         this.title = title;
     }
 
+    // this just returns the title concatenated with the string "Title"
+    // keep in mind only one return statement can exist in a function
     toString()
     {
         return "Title: " + this.title;    
     }
 
+    // this is the property theTitle which returns the title as well only the title
     get theTitle()
     {
         return this.title;
@@ -19,19 +26,28 @@ class ViewFinder
 
 }
 
+// this function is called in the body of the HTML page so that the objects are created and added to the 
+// array myViewFinderArray
 function initializeArray()
 {
+    // create the first object from the class ViewFinder
     var myViewFinder = new ViewFinder("A really funny Pug");
+    // create a second object from the class ViewFinder
     var myViewFinder1 = new ViewFinder("A really angry tiger");
+    // add the first object to the array
     myViewFinderArray.push(myViewFinder);
+    // add the second object to the array
     myViewFinderArray.push(myViewFinder1);
 
 }
+
+// this function gets an object from the array and puts it into the element with the id title 
 function accessInformation()
 {
-    /// random object from the array
-    //console.log(myViewFinder.toString());
-    //console.log(myViewFinder.theTitle);
-    document.getElementById("title").innerHTML = myViewFinderArray[1].toString();
+    // get a random number
+    var randomNumber = Math.floor(Math.random() * myViewFinderArray.length);
+    // get a random object from the array
+    // calling the toString, but we could have gotten the property theTitle as well
+    document.getElementById("title").innerHTML = myViewFinderArray[randomNumber].toString();
 
 }
